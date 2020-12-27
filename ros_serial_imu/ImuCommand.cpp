51211b2c2d3447eaade6f6226bf90c652b839d6b
Tarmy3124 <1498860484@qq.com>
2020-12-27 04:58:07 -0800

    Odom/IMU

---
+++ ros_serial_imu/ImuCommand.cpp
@@ -0,0 +1,355 @@
+#include"ImuCommand.h"
+
+ros::Publisher msg_pub;
+ros::Subscriber pid_sub;
+ros::Publisher odom_pub;  
+
+//Convert float to char and send it through serial port
+void IPSG::CImuCommand::float2char(float f,unsigned char *s)
+{
+ unsigned char *p;
+ p = (unsigned char *)&f;
+
+    *s = *p;
+
+    *(s+1) = *(p+1);
+
+    *(s+2) = *(p+2);
+
+    *(s+3) = *(p+3);
+
+}
+void IPSG::CImuCommand::char2float(float *f,unsigned char *s)
+{
+    //f = (float *)s;
+    memcpy(f, s, 4);
+}
+
+bool IPSG::CImuCommand::display_decodeData(double *tmpBuffer)
+{
+    for(int i = 0;i < DATA_LENGTH;i ++)
+    {
+        std::cout<<tmpBuffer[i]<<std::endl;
+    }
+    return true;
+}
+
+bool IPSG::CImuCommand::display_Q4decodeData(double tmpBuffer[Q4DATA_LENGTH])
+{
+    for(int i = 0;i < Q4DATA_LENGTH;i ++)
+    {
+        std::cout<<tmpBuffer[i]<<std::endl;
+    }
+    return true;
+}
+
+bool IPSG::CImuCommand::display_Imumsg(sensor_msgs::Imu imumsg)
+{   
+   /* std::cout<<"print Q4 data!"<<std::endl;
+    std::cout<<imumsg.orientation.x<<std::endl;
+    std::cout<<imumsg.orientation.y<<std::endl;
+    std::cout<<imumsg.orientation.z<<std::endl;
+    std::cout<<imumsg.orientation.w<<std::endl;*/
+
+    std::cout<<"print GRY data!"<<std::endl;
+    std::cout<<imumsg.angular_velocity.x<<std::endl;
+    std::cout<<imumsg.angular_velocity.y<<std::endl;
+    std::cout<<imumsg.angular_velocity.z<<std::endl;
+
+    std::cout<<"print ACC data!"<<std::endl;
+    std::cout<<imumsg.linear_acceleration.x<<std::endl;
+    std::cout<<imumsg.linear_acceleration.y<<std::endl;
+    std::cout<<imumsg.linear_acceleration.z<<std::endl;
+}
+
+bool IPSG::CImuCommand::decodeFrame(unsigned char tmpBuffer[READ_BUFFERSIZE])
+{
+    if(tmpBuffer[0] == DATA_FrameHead && tmpBuffer[1] == DATA_FrameHead)
+    {
+        imu_data.header.frame_id = "imu_link";
+ 	imu_data.header.stamp = ros::Time::now();
+        switch(tmpBuffer[2])
+        {
+            case DATA_TYPE_Q4:  //该帧输出数据为四元数类型
+                { 
+                   /* Q4[0] = (double)(tmpBuffer[4]<<8|tmpBuffer[5])/Q4_MEASURE;
+                    Q4[1] = (double)(tmpBuffer[6]<<8|tmpBuffer[7])/Q4_MEASURE;
+                    Q4[2] = (double)(tmpBuffer[8]<<8|tmpBuffer[9])/Q4_MEASURE;
+                    Q4[3] = (double)(tmpBuffer[10]<<8|tmpBuffer[11])/Q4_MEASURE;
+                    display_Q4decodeData(Q4);
+                    
+                    imu_data.orientation.x = Q4[1];
+                    imu_data.orientation.y = Q4[2];
+                    imu_data.orientation.z = Q4[3];
+                    imu_data.orientation.w = Q4[0];*/
+                    
+                    break;
+                }
+
+
+            case DATA_TYPE_GRY: //该帧输出数据为陀螺仪数据类型
+                {                
+                    GYR[0] = (short)(((uint16_t)tmpBuffer[4]<<8)|tmpBuffer[5]);
+                    GYR[1] = (short)(((uint16_t)tmpBuffer[6]<<8)|tmpBuffer[7]);
+                    GYR[2] = (short)(((uint16_t)tmpBuffer[8]<<8)|tmpBuffer[9]);
+                    display_decodeData(GYR);
+                    imu_data.angular_velocity.x = (GYR[0]/16.04*M_PI/180);
+                    imu_data.angular_velocity.y = (GYR[1]/16.04*M_PI/180);
+                    imu_data.angular_velocity.z = (GYR[2]/16.04*M_PI/180);
+
+                    break;
+                }
+
+            case DATA_TYPE_ACC:  //该帧输出数据为加速度类型
+                 { 
+                     ACC[0] = (short)((uint16_t)tmpBuffer[4]<<8|tmpBuffer[5]);
+                     ACC[1] = (short)((uint16_t)tmpBuffer[6]<<8|tmpBuffer[7]);
+                     ACC[2] = (short)((uint16_t)tmpBuffer[8]<<8|tmpBuffer[9]);
+                     display_decodeData(ACC);
+                     imu_data.linear_acceleration.x = (ACC[0]/2048*9.98);
+                     imu_data.linear_acceleration.y = (ACC[1]/2048*9.98);
+                     imu_data.linear_acceleration.z = (ACC[2]/2048*9.98);
+                     
+                     break;
+                 } 
+            case DATA_TYPE_ODOM_Vx:
+                 {
+                     odom_Vx_char[0] = tmpBuffer[4];
+                     odom_Vx_char[1] = tmpBuffer[5];
+                     odom_Vx_char[2] = tmpBuffer[6];
+                     odom_Vx_char[3] = tmpBuffer[7];
+                     IPSG::CImuCommand::char2float(&Vx_float,odom_Vx_char);
+
+                 } 
+            case DATA_TYPE_ODOM_Vy:
+                 {
+                     odom_Vy_char[0] = tmpBuffer[4];
+                     odom_Vy_char[1] = tmpBuffer[5];
+                     odom_Vy_char[2] = tmpBuffer[6];
+                     odom_Vy_char[3] = tmpBuffer[7];
+                     IPSG::CImuCommand::char2float(&Vy_float,odom_Vy_char);
+
+                 } 
+            case DATA_TYPE_ODOM_Vz:
+                 {
+                     odom_Vz_char[0] = tmpBuffer[4];
+                     odom_Vz_char[1] = tmpBuffer[5];
+                     odom_Vz_char[2] = tmpBuffer[6];
+                     odom_Vz_char[3] = tmpBuffer[7];
+                     IPSG::CImuCommand::char2float(&Vz_float,odom_Vz_char);
+                 } 
+
+            case DATA_TYPE_MAG:  {std::cout<<"未更新DATA_TYPE_MAG解码程序！"<<std::endl;break;}   //该帧输出数据为磁力计类型
+
+            case DATA_TYPE_EULAR:  //解码欧拉角原始数据
+                { 
+                     EULAR[0] = (double)(tmpBuffer[4]<<8|tmpBuffer[5])/EULAR_MEASURE;
+                     EULAR[1] = (double)(tmpBuffer[6]<<8|tmpBuffer[7])/EULAR_MEASURE;
+                     EULAR[2] = (double)(tmpBuffer[8]<<8|tmpBuffer[9])/EULAR_MEASURE;
+                     display_decodeData(EULAR);
+                     break;
+                }
+
+            case DATA_TYPE_STOP: {std::cout<<"未更新DATA_TYPE_STOP解码程序！"<<std::endl;break;} //该帧输出数据为保留不用类型
+
+            case DATA_TYPE_PRECISION:{std::cout<<"未更新DATA_TYPE_PRECISION解码程序！"<<std::endl;break;}  //该帧输出数据为传感器精度，频率类型
+
+            case DATA_TYPE_RANGE: {std::cout<<"未更新DATA_TYPE_RANGE解码程序！"<<std::endl;break;}  //该帧输出数据为传感器量程
+            default: break;
+        }
+
+    }
+    else
+    {
+        std::cout<<"数据帧头错误"<<std::endl;
+        return false;
+    }
+                msg_pub.publish(imu_data);
+    return true;
+}
+
+
+bool IPSG::CImuCommand::cmdFrame(unsigned char imucmd)
+{
+    cmd_buffer[0] = CMD_FrameHead;
+    cmd_buffer[1] = imucmd;
+    cmd_buffer[2] = CMD_FrameHead + cmd_buffer[1];
+    //return commd_buffer;
+
+    //ser.write(cmd_buffer,cmd_num);
+    int i=0;
+    ser.read(r_buffer,READ_BUFFERSIZE);
+    if(r_buffer[12]!=DATA_TYPE_STOP)
+    {
+      for (;i<26;i++)
+     {
+      if(r_buffer[i]=DATA_TYPE_STOP)break;
+
+     }
+    ser.read(r_buffer_helper,READ_BUFFERSIZE+i+1);
+    r_buffer_handled=(&r_buffer_helper[i+1]);
+    decodeFrame(r_buffer_handled);
+   ROS_INFO("The message was truncated");
+    }else
+    decodeFrame(r_buffer);
+    return true;
+}
+
+
+bool IPSG::CImuCommand::muliteCmdFrame(unsigned char imucmd1,unsigned char imucmd2,unsigned char imucmd3)
+{
+    //第一个指令
+   // cmdFrame(imucmd1);
+    //ROS_INFO("OUTPUT_Q4");   //如何输出宏定义的名字？？？？
+
+    //第二个指令
+    //cmdFrame(imucmd2);
+    ROS_INFO("OUTPUT_GYR");
+
+    //第三个指令
+    cmdFrame(imucmd3);
+    ROS_INFO("OUTPUT_ACC");
+
+return true;
+}
+
+
+bool IPSG::CImuCommand::serialInit()
+{
+        try
+    {
+        ser.setPort("/dev/ttyUSB0");
+        ser.setBaudrate(115200);
+        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+        ser.setTimeout(to);
+        ser.open();
+    }
+    catch (serial::IOException& e)
+    {
+        ROS_ERROR_STREAM("Unable to open port ");
+        return -1;
+    }
+
+    if(ser.isOpen()){
+        ROS_INFO_STREAM("Serial Port initialized");
+       // cmdFrame(CMD_OUTPUT_200HZ);
+        //ser.write(cmd_buffer,cmd_num);
+    }else{
+        return -1;
+    }
+    return true;
+}
+
+void IPSG::CImuCommand::pid_write_callback(const carMsgs::pidPtr &pid_msg)
+{
+     int i=0;
+     float pid[3];
+     pid[0]=pid_msg->k1_p; 
+     pid[1]=pid_msg->k1_i;
+     pid[2]=pid_msg->k1_d;
+     for (;i<3;i++){
+     IPSG::CImuCommand::float2char(pid[i],&pid_char_buffer[4*i]);
+     }
+     if(ser.isOpen()){
+        ROS_INFO_STREAM("Serial Port initialized");
+       // cmdFrame(CMD_OUTPUT_200HZ);
+        ser.write(pid_char_buffer,13);
+      }else{
+        ROS_INFO("Serial Port has not been opened");
+      }
+}
+bool IPSG::CImuCommand::RUN()
+{   
+    ros::NodeHandle nh;
+    
+    int odom_count;
+   // ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
+    msg_pub = nh.advertise<sensor_msgs::Imu>("imu", 100);
+    odom_pub =nh.advertise<nav_msgs::Odometry>("odom", 10);
+    tf::TransformBroadcaster odom_broadcaster;
+    pid_sub = nh.subscribe("pid_float",200,&IPSG::CImuCommand::pid_write_callback,this);
+    //串口初始化
+    serialInit();
+    ros::Rate loop_rate(500);
+    pid_char_buffer[12]='\r';
+    //for odom 
+    ros::Time current_time, last_time;
+    current_time = ros::Time::now();
+    last_time = ros::Time::now();
+
+    while(ros::ok())
+    {
+
+            muliteCmdFrame(CMD_PULL_OUTPUT_Q4,CMD_PULL_OUTPUT_GYR,CMD_PULL_OUTPUT_ACC);
+            display_Imumsg(imu_data);
+            //msg_pub.publish(imu_data);
+            ros::spinOnce();
+            //for odom
+            current_time = ros::Time::now();
+            odom_count++;
+            if(odom_count>10)
+            {
+             odom_count=0;
+            //compute odometry in a typical way given the velocities of the robot
+            float dt = (current_time - last_time).toSec();
+            float delta_x = (Vx_float * cos(th)) * dt;
+            float delta_y = (Vy_float * sin(th)) * dt;
+            float delta_th = Vz_float * dt;
+
+             x += delta_x;
+             y += delta_y;
+             th += delta_th;
+             //since all odometry is 6DOF we'll need a quaternion created from yaw
+            geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+            //tf提供的功能允许从yaw(偏航值)容易地创建四元数，并且可以方便地获取四元数的偏航值。     
+             //first, we'll publish the transform over tf
+             geometry_msgs::TransformStamped odom_trans;
+             odom_trans.header.stamp = current_time;
+             odom_trans.header.frame_id = "odom";
+             odom_trans.child_frame_id = "base_link";
+             /*在这里，我们将创建一个TransformStamped消息，通过tf发送。
+            我们要发布在current_time时刻的从"odom"坐标系到“base_link”坐标系的变换。
+            因此，我们将相应地设置消息头和child_frame_id，
+            确保使用“odom”作为父坐标系，将“base_link”作为子坐标系。*/
+ 
+            odom_trans.transform.translation.x = x;
+            odom_trans.transform.translation.y = y;
+            odom_trans.transform.translation.z = 0.0;
+            odom_trans.transform.rotation = odom_quat;
+ 
+            //send the transform
+            odom_broadcaster.sendTransform(odom_trans);
+            //将我们的里程数据中填入变换消息中，然后使用TransformBroadcaster发送变换。
+ 
+            //next, we'll publish the odometry message over ROS
+            nav_msgs::Odometry odom;
+        odom.header.stamp = current_time;
+        odom.header.frame_id = "odom";
+        //还需要发布nav_msgs/Odometry消息，以便导航包可以从中获取速度信息。
+        //将消息的header设置为current_time和“odom”坐标系。
+        //set the position
+        odom.pose.pose.position.x = x;
+        odom.pose.pose.position.y = y;
+        odom.pose.pose.position.z = 0.0;
+        odom.pose.pose.orientation = odom_quat;
+ 
+        //set the velocity
+        odom.child_frame_id = "base_link";
+        odom.twist.twist.linear.x = Vx_float;
+        odom.twist.twist.linear.y = Vy_float;
+        odom.twist.twist.angular.z= Vz_float;
+        //这将使用里程数据填充消息，并发送出去。
+        //我们将消息的child_frame_id设置为“base_link”坐标系，
+        //因为我们要发送速度信息到这个坐标系。
+ 
+        //publish the message
+        odom_pub.publish(odom);
+        last_time = current_time;
+    }
+
+
+            loop_rate.sleep();
+    }
+
+        return true;
+}
---
+++ ros_serial_imu/ImuCommand.h
@@ -0,0 +1,209 @@
+/************************************************************************************************************
+ *  Copyright Notice
+ *  Copyright (c) 2018,冯
+ *
+ *  @File    : ImuCommand.h
+ *  @Brief   : IMu指令
+ *
+ *  @Version : V1.0
+ *  @Date    : 2018/11/23
+
+ *	@History :
+ *    Author :
+ *    Descrip: 命令字节，由外部控制器发送至GY953，帧头0xa5,指令格式：帧头+指令+校验和
+ *************************************************************************************************************/
+
+#ifndef IMUCOMMAND_H
+#define IMUCOMMAND_H
+
+#include <iostream>
+#include "serial/serial.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+#include "odom_serial/serial.h"
+#include <sensor_msgs/Imu.h>
+#include "carMsgs/pid.h"
+
+//for odom
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+
+#define CMD_FrameHead								   165		   //指令帧头
+#define READ_BUFFERSIZE                                 13         //读取数据数组长度
+#define cmd_num                                          3         //指令数组长度
+#define PIDCHAR_NUM                                     13         //pid串口字符缓冲数量
+#define DATA_LENGTH                                      3         //解码后数组长度
+#define Q4DATA_LENGTH                                    4         //解码后四元数数组长度
+#define mulitCmd_num                                     9         //复合指令数组长度
+#define EULAR_MEASURE                                100.0         //欧拉角度量，原始数据除以之
+#define Q4_MEASURE                                  1000.0         //四元数度量，原始数据除以之
+
+
+//sago Odom
+#define DATA_TYPE_ODOM_Vx                   (unsigned char)(40)   //odom vx
+#define DATA_TYPE_ODOM_Vy                   (unsigned char)(41)   //odom vy
+#define DATA_TYPE_ODOM_Vz                   (unsigned char)(42)   //odom vz
+
+
+//判断本帧数据类型
+#define DATA_FrameHead                   (unsigned char)(90)       //接收数据的帧头
+#define DATA_TYPE_ACC                    (unsigned char)(21)       //该帧输出数据为加速度类型
+#define DATA_TYPE_GRY                    (unsigned char)(37)       //该帧输出数据为陀螺仪类型
+#define DATA_TYPE_MAG                    (unsigned char)(53)       //该帧输出数据为磁力计类型
+#define DATA_TYPE_EULAR                  (unsigned char)(69)       //该帧输出数据为欧拉角类型
+#define DATA_TYPE_STOP                   (unsigned char)(85)       //该帧输出数据为保留不用类型
+#define DATA_TYPE_Q4                     (unsigned char)(101)      //该帧输出数据为四元数类型
+#define DATA_TYPE_PRECISION              (unsigned char)(117)      //该帧输出数据为传感器精度，频率类型
+#define DATA_TYPE_RANGE                  (unsigned char)(133)      //该帧输出数据为传感器量程
+
+
+
+
+//串口波特率设置指令，每次修改后需要重新上电生效，具有掉电保持特性
+#define CMD_SET_BAUD115200               (unsigned char)(175)      //115200(默认)
+#define CMD_SET_BAUD9600                 (unsigned char)(174)      //9600
+
+//传感器配置指令
+#define  CMD_CLOSE_JIAJI                 (unsigned char)(81)     //关闭加计
+#define  CMD_CLOSE_GYR                   (unsigned char)(82)     //关闭陀螺
+#define  CMD_CLOSE_MAG                   (unsigned char)(83)     //关闭磁场
+#define  CMD_GYR_CALIBRATE               (unsigned char)(87)     //加陀校准
+#define  CMD_MAG_CALIBRATE               (unsigned char)(88)     //磁场校准
+#define  CMD_RES_FACTORY                 (unsigned char)(89)     //恢复出厂设置，即清除保存的校准数据
+#define  CMD_OUTPUT_50HZ                 (unsigned char)(164)    //模块输出频率50hz设置
+#define  CMD_OUTPUT_100HZ                (unsigned char)(165)    //模块输出频率100hz设置
+#define  CMD_OUTPUT_200HZ                (unsigned char)(166)    //模块输出频率200hz设置
+
+//自动输出指令（具有开关功能，第一次发送打开，第二次发送关闭）
+#define  CMD_OUTPUT_ACC                  (unsigned char)(21)     //输出加速度原始数据指令
+#define  CMD_OUTPUT_GYR                  (unsigned char)(37)     //输出陀螺仪原始数据
+#define  CMD_OUTPUT_MAG                  (unsigned char)(53)     //输出磁场原始数据指令
+#define  CMD_OUTPUT_EULAR                (unsigned char)(69)     //输出欧拉角指令（默认50HZ）
+#define  CMD_OUTPUT_CHAREULAR            (unsigned char)(85)     //输出欧拉角字符形式（串口助手）
+#define  CMD_OUTPUT_Q4                   (unsigned char)(101)    //输出四元数数据指令
+
+//查询输出指令
+#define  CMD_READ_CALIBRATE_PRECISION    (unsigned char)(117)     //读取三传感器校准精度，输出频率
+#define  CMD_READ_RANGE                  (unsigned char)(133)     //读取传感器量程，获取陀螺量程，加计量程，磁场量程
+#define  CMD_OPENCV_ALLSENSOR            (unsigned char)(80)      //开启所有传感器
+#define  CMD_PULL_OUTPUT_EULAR           (unsigned char)(149)     //输出欧拉角指令（默认50HZ）
+#define  CMD_PULL_OUTPUT_Q4              (unsigned char)(181)     //输出四元数数据指令
+#define  CMD_PULL_OUTPUT_ACC             (unsigned char)(197)     //输出加速度原始数据指令
+#define  CMD_PULL_OUTPUT_GYR             (unsigned char)(213)     //输出陀螺仪原始数据
+#define  CMD_PULL_OUTPUT_MAG             (unsigned char)(229)     //输出磁场原始数据指令
+
+
+extern ros::Publisher msg_pub;
+extern ros::Subscriber pid_sub;
+//extern ros::Publisher odom_pub; 
+//extern tf::TransformBroadcaster odom_broadcaster;
+namespace IPSG
+{   
+    
+	class CImuCommand
+	{
+        
+        serial::Serial ser;
+		sensor_msgs::Imu imu_data;
+	public:
+
+		CImuCommand(){};
+
+		~CImuCommand(){};
+               void float2char(float f,unsigned char *s);
+               void char2float(float *f,unsigned char *s);
+        
+
+		/***********************************************
+		 *  @Name    : CImuCommand::cmdFrame
+		 *  @Descrip : 向IMU发送一帧指令
+         *  @in      : 指令
+         *  @out     : 原始数据
+		 *  @Return  : bool true:success false:failed
+		 *  @Notes   : None
+		 ***********************************************/
+		bool cmdFrame(unsigned char imucmd);
+		/***********************************************
+		 *  @Name    : CImuCommand::muliteCmdFrame
+		 *  @Descrip : 向IMU发送三个指令，查询输出
+		 *  @Para    : [in]三个指令
+		 *	@Para    : [in/out]	v_ucData：原始数据帧
+		 *  @Return  : bool true:success false:failed
+		 *  @Notes   : None
+		 ***********************************************/
+		bool muliteCmdFrame(unsigned char imucmd1,unsigned char imucmd2,unsigned char imucmd3);
+
+		/***********************************************
+		 *  @Name    : CImuCommand::decodeFrame
+		 *  @Descrip : 解码
+		 *  @Notes   : None
+		 ***********************************************/
+        bool decodeFrame(unsigned char tmpBuffer[READ_BUFFERSIZE]);
+
+		/***********************************************
+		 *  @Name    : CImuCommand::display_Imumsg
+		 *  @Descrip : 打印imu.msg消息类型
+		 *  @para    : [in]sensor::Imu 数据类型
+		 *  @Notes   : None
+		 ***********************************************/
+		bool display_Imumsg(sensor_msgs::Imu imumsg);
+
+		/***********************************************
+		 *  @Name    : CImuCommand::serialInit
+		 *  @Descrip : 串口初始化
+		 *  @Notes   : None
+		 ***********************************************/
+		bool serialInit();
+
+		/***********************************************
+		 *  @Name    : CImuCommand::serialInit
+		 *  @Descrip : 总运行程序
+		 *  @Notes   : None
+		 ***********************************************/
+		bool RUN();
+                void pid_write_callback(const carMsgs::pidPtr &pid_msg);
+
+
+		/***********************************************
+		 *  @Name    : CImuCommand::display_decodeData
+		 *  @Descrip : 打印角速度、线加速度
+		 *  @Notes   : None
+		 ***********************************************/
+        bool display_decodeData(double tmpBuffer[DATA_LENGTH]);
+				/***********************************************
+		 *  @Name    : CImuCommand::display_Q4decodeData
+		 *  @Descrip : 打印四元数
+		 *  @Notes   : None
+		 ***********************************************/
+        bool display_Q4decodeData(double tmpBuffer[Q4DATA_LENGTH]);
+    private:
+       // class Serial *mSerial;
+
+
+	public:
+        unsigned char                pid_char_buffer[PIDCHAR_NUM];
+        unsigned char                cmd_buffer[cmd_num];
+        unsigned char     mulit_cmd_buffer[mulitCmd_num];
+        unsigned char          r_buffer[READ_BUFFERSIZE];
+        unsigned char          r_buffer_helper[26];
+        unsigned char          *r_buffer_handled=NULL;
+        unsigned char          odom_Vx_char[4];
+        unsigned char          odom_Vy_char[4];
+        unsigned char          odom_Vz_char[4];
+        double                        EULAR[DATA_LENGTH];
+        double              read_buffer[READ_BUFFERSIZE];
+        double                          GYR[DATA_LENGTH];
+        double                         Q4[Q4DATA_LENGTH];
+        double                          ACC[DATA_LENGTH];
+        float                 Vx_float;
+        float                 Vy_float;
+        float                 Vz_float;
+        float                 x,y,th;
+		
+	};
+
+
+}
+
+#endif //IMUCOMMAND_H
